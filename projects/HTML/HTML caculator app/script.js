a34bd2ae3caba7255efc5118d505d557d1e17264
Kurt Alwin D. Rances <113751575+usagikatto@users.noreply.github.com>
2025-07-15 21:26:36 +0800

    update javascript

--- projects/HTML/HTML caculator app/script.js
+++ projects/HTML/HTML caculator app/script.js
@@ -2,5 +2,33 @@ const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator__keys')
 
 keys.addEventListener('click', e => {
-    
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+
+        if (!action) {
+            console.log('number key!')
+        }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divde'
+        ) {
+            console.log('operator key!')
+        }
+
+        if (action === 'decimal') {
+            console.log('decimal key!')
+        }
+
+        if (action === 'clear') {
+            console.log('clear key!')
+        }
+
+        if (action === 'calcualte') {
+            console.log('equals key!')
+        }
+    }
 })
